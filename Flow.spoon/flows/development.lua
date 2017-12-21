0d7783c1cfaac10d7922cc414f622cf13746eb47
Daniel Brady <daniel.brady@tapjoy.com>
2017-12-21 18:24:17 -0500

    Refactor Dev flow in accordance with base Flow changes

--- Flow.spoon/flows/development.lua
+++ Flow.spoon/flows/development.lua
@@ -1,22 +1,19 @@
-BaseWorkflow = ... -- Expect our base object to be passed in while loading this file.
-dev = BaseWorkflow.new('Development')
-
-dev:setActionPalette({
-  {
-    name = 'Open project',
-    command = function()
-      local tapjoyProjectRoot = '~/github/tapjoy/'
-      local tapjoyProjects = hs.chooser.generateChoiceTable(hs.fs.dirs(tapjoyProjectRoot))
-      hs.chooser.new(
-        function(project)
-          if project ~= nil then
-            local projectPath = tapjoyProjectRoot..project.text
-            hs.execute('atom '..projectPath, true)
+local BaseFlow = ... -- Expect our base object to be passed in while loading this file.
+return BaseFlow.new('Development')
+  :setActionPalette({
+    {
+      name = 'Open project',
+      command = function()
+        local tapjoyProjectRoot = '~/github/tapjoy/'
+        local tapjoyProjects = hs.chooser.generateChoiceTable(hs.fs.dirs(tapjoyProjectRoot))
+        hs.chooser.new(
+          function(project)
+            if project ~= nil then
+              local projectPath = tapjoyProjectRoot..project.text
+              hs.execute('atom '..projectPath, true)
+            end
           end
-        end
-      ):choices(tapjoyProjects):show()
-    end
-  }
-})
-
-hs.hotkey.bind({'cmd', 'shift'}, '`', function() dev:enter() end)
+        ):choices(tapjoyProjects):show()
+      end
+    }
+  })
